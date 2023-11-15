e4697f9f04304eb4591bec8d6cd5aa01a94f2043
Jack Blute <107153158+jb318@users.noreply.github.com>
2023-11-14 20:09:04 -0500

    All tests passing

--- src/caesar.js
+++ src/caesar.js
@@ -1,60 +1,25 @@
-// Please refrain from tampering with the setup code provided here,
-// as the index.html and test files rely on this setup to work properly.
-// Only add code (helper methods, variables, etc.) within the scope
-// of the anonymous function on line 6
+function caesarRenderer() {
+  const form = document.querySelector("#caesar");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-const caesarModule = (function () {
-  function caesar(input, shift, encode = true) {
-    if (shift === 0 || shift < -25 || shift > 25 || shift === undefined) {
-      return false
+    const input = event.target["caesar-input"].value;
+    const direction = event.target["caesar-options"].value;
+    const shift = event.target["caesar-shift"].value;
+    const result =
+      direction === "encode"
+        ? caesarModule.caesar(input, Number(shift))
+        : caesarModule.caesar(input, Number(shift), false);
+
+    const alert = document.querySelector("#caesar-alert");
+    if (result) {
+      alert.classList.add("d-none");
+      const output = document.querySelector("#caesar-output");
+      output.innerHTML = result;
+    } else {
+      alert.classList.remove("d-none");
     }
-    const message = []
-    const caesarShift = []
-    let caesarMessage = ""
-    for (let i = 0; i < input.length; i++) {
-      message.push(input.slice(i, i + 1))
-    }
-    const shiftMessage = message.filter((messageShift,index) => {
-      if (messageShift.charCodeAt() >= 65 && messageShift.charCodeAt() <= 90) {
-        if (messageShift.charCodeAt() + shift >= 65 && messageShift.charCodeAt() + shift <= 90) {
-          console.log("Upper Case")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift)).toLowerCase())
-        }
-        else if (messageShift.charCodeAt() + shift > 90){
-          console.log("Upper Case - Shift Exceeds Alphabet")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift - 26)).toLowerCase())
-        }
-        else {
-          console.log("Upper Case - Shift Less Than Alphabet")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift + 26)).toLowerCase())
-        }
-      }
-      else if (messageShift.charCodeAt() >= 97 && messageShift.charCodeAt() <= 122) {
-        if (messageShift.charCodeAt() + shift >= 97 && messageShift.charCodeAt() + shift <= 122) {
-          console.log("Lower Case")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift)))
-        }
-        else if (messageShift.charCodeAt() + shift > 122) {
-          console.log("Lower Case - Shift Exceeds Alphabet")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift - 26)))
-        }
-        else {
-          console.log("Lower Case - Shift Less Than Alphabet")
-          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift + 26)))
-        }
-      }
-      else {
-        console.log("Special Character")
-        caesarShift.push(messageShift)
-      }
-      caesarMessage += caesarShift[index]
-    })
-    return caesarMessage
-  }
-  return {
-    caesar
-  }
-})()
+  });
+}
 
-
-module.exports = { caesar: caesarModule.caesar };
+document.addEventListener("DOMContentLoaded", caesarRenderer);
