18307ba0884661b8ace9a9c4fc6d845b28536d20
Jack Blute <107153158+jb318@users.noreply.github.com>
2023-11-14 20:10:27 -0500

    Committed the wrong file lmao

--- src/caesar.js
+++ src/caesar.js
@@ -1,25 +1,61 @@
-function caesarRenderer() {
-  const form = document.querySelector("#caesar");
-  form.addEventListener("submit", (event) => {
-    event.preventDefault();
+// Please refrain from tampering with the setup code provided here,
+// as the index.html and test files rely on this setup to work properly.
+// Only add code (helper methods, variables, etc.) within the scope
+// of the anonymous function on line 6
 
-    const input = event.target["caesar-input"].value;
-    const direction = event.target["caesar-options"].value;
-    const shift = event.target["caesar-shift"].value;
-    const result =
-      direction === "encode"
-        ? caesarModule.caesar(input, Number(shift))
-        : caesarModule.caesar(input, Number(shift), false);
-
-    const alert = document.querySelector("#caesar-alert");
-    if (result) {
-      alert.classList.add("d-none");
-      const output = document.querySelector("#caesar-output");
-      output.innerHTML = result;
-    } else {
-      alert.classList.remove("d-none");
+const caesarModule = (function () {
+  function caesar(input, shift, encode = true) {
+    // return false if there is any unwanted functionality such as shift value being 0
+    if (shift === 0 || shift < -25 || shift > 25 || shift === undefined) {
+      return false
     }
-  });
-}
+    // properly adjust shift to be either positive or negative depending on the truth value of encode
+    if (encode === false) {
+      shift = shift * -1 
+    }
+    // create an array that contains each character of the input at a specific index, an array that contains the shift value of those indices, and a string variable that contains the decoded message
+    const message = []
+    const caesarShift = []
+    let caesarMessage = ""
+    for (let i = 0; i < input.length; i++) {
+      message.push(input.slice(i, i + 1))
+    }
+    // loop through the message array and append to the shift array as intended by the instructions
+    const shiftMessage = message.filter((messageShift,index) => {
+      if (messageShift.charCodeAt() >= 65 && messageShift.charCodeAt() <= 90) {
+        if (messageShift.charCodeAt() + shift >= 65 && messageShift.charCodeAt() + shift <= 90) {
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift)).toLowerCase())
+        }
+        else if (messageShift.charCodeAt() + shift > 90){
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift - 26)).toLowerCase())
+        }
+        else {
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift + 26)).toLowerCase())
+        }
+      }
+      else if (messageShift.charCodeAt() >= 97 && messageShift.charCodeAt() <= 122) {
+        if (messageShift.charCodeAt() + shift >= 97 && messageShift.charCodeAt() + shift <= 122) {
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift)))
+        }
+        else if (messageShift.charCodeAt() + shift > 122) {
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift - 26)))
+        }
+        else {
+          caesarShift.push(String.fromCharCode((messageShift.charCodeAt() + shift + 26)))
+        }
+      }
+      else {
+        caesarShift.push(messageShift)
+      }
+      // don't forget to update the decoded message string variable
+      caesarMessage += caesarShift[index]
+    })
+    return caesarMessage
+  }
+  return {
+    caesar
+  }
+})()
 
-document.addEventListener("DOMContentLoaded", caesarRenderer);
+
+module.exports = { caesar: caesarModule.caesar };
